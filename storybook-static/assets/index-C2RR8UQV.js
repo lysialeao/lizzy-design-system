5151c50529b484dd0f208f94b7bbea87be855ead
lysialeao <lysialeao@gmail.com>
2024-08-12 09:58:26 -0300

    build: :rocket: deploy settings

---
+++ storybook-static/assets/index-C2RR8UQV.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-DsqykDs0.js";import"./index-CvCkEiTP.js";import"./iframe-DCdz5Yin.js";import"../sb-preview/runtime.js";import"./index-TAVom-gR.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
